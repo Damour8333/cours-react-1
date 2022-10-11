d5e1bb452bb8e1e5492405a9b013b2563d61a0d9
Damour8333 <98494743+Damour8333@users.noreply.github.com>
2022-10-11 16:09:48 +0200

    router

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores id possimus, perferendis minus harum aperiam adipisci! Suscipit quod impedit nostrum dolor ad distinctio explicabo officia laudantium error, non natus eaque doloremque debitis sunt quisquam. Sed alias ipsa veritatis doloribus, magni iusto voluptatum nostrum officiis obcaecati cumque! Et vel impedit voluptatibus, dignissimos, maiores labore ipsum nihil id voluptas atque perspiciatis nemo harum aspernatur! Error iste mollitia ad quae veritatis, hic atque provident voluptates earum non totam eum neque maxime accusantium consequuntur odio dolorum natus minus repellendus sit minima aliquam tempora ducimus explicabo! Cumque voluptatem consequatur sit dolorem fugiat ducimus deserunt eius.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem quam soluta minima ipsa nisi ullam, qui ratione quisquam iste iusto tempore itaque natus aperiam? Dicta voluptas provident officia, similique nam aperiam. Dolores hic veniam nostrum aperiam nobis, esse corporis doloribus tempora quia voluptas labore, placeat impedit. Doloremque unde consectetur obcaecati facilis vero ex vitae suscipit officia hic adipisci quis saepe eligendi blanditiis veritatis ducimus necessitatibus illum at eaque tempore, consequuntur ipsam, debitis repudiandae architecto perferendis. Autem minus iste necessitatibus distinctio assumenda nam voluptate blanditiis, neque cupiditate sit, nihil vel maxime eius commodi? Non incidunt iure ducimus consectetur eligendi unde pariatur doloribus modi. Molestias a ea culpa eligendi praesentium iusto, veritatis nihil porro corrupti sit tempore cum. Reiciendis odit neque autem inventore voluptate amet, incidunt tempora saepe blanditiis sunt voluptatem sapiente rem iusto laborum aliquam voluptates consectetur molestias tempore magnam eveniet itaque eos! Corporis dolore laborum quae quisquam obcaecati voluptatibus modi nisi rem beatae saepe voluptatum incidunt, ad repellat temporibus pariatur reprehenderit nesciunt iste! Tenetur, porro iste nemo dolorem itaque possimus autem aperiam ipsa sunt fugit eius nobis, dolores ab ratione, consectetur expedita fuga nihil quod eligendi aliquid! Laudantium non dicta animi nihil beatae. Nobis quisquam officiis voluptate molestias sed.</p>
+        </div>
+    );
+};
+
+export default About;
