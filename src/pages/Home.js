ff211ba7a9a27d7c5d19d4f10df91c42a0159528
Damour8333 <98494743+Damour8333@users.noreply.github.com>
2022-10-13 11:13:55 +0200

    hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Countries from '../components/Countries';
+import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const Home = () => {
     return (
         <div>
+            <Logo />
             <Navigation />
+            <Countries />
             <h1>ACCEUIL</h1>
         </div>
     );
